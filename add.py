9cbd7a93454656b25acef498d246ca65792b737c
Amit Tomar <49426082+amittomar-1@users.noreply.github.com>
2020-04-03 14:23:02 +0530

    Add files via upload

---
+++ add.py
@@ -0,0 +1,8 @@
+# Program to add 2 nummbers
+
+num1 = 100
+num2 = 6
+
+sum = num1 + num2
+
+print('The sum of {0} and {1} is {2}'.format(num1, num2, sum))
---
+++ add_by_user.py
@@ -0,0 +1,8 @@
+# store and add
+
+num1 = input('input the first number')
+num2 = input('input the second number')                                                                                 
+
+sum = int(num1) + int(num2)
+
+print ('the sum of {0} and {1} is {2}'.format(num1, num2, sum))
